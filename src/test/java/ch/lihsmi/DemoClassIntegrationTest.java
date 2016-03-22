8833751d81bb8d939441a611a3f5ffcc916912fa
Michael Lihs <lihs@punkt.de>
2016-03-22 16:37:56 +0100

    [TASK] Add a sample integration test
    Run the integration test with
    
        mvn clean test -P IntegrationTests

---
+++ src/test/java/ch/lihsmi/DemoClassIntegrationTest.java
@@ -0,0 +1,17 @@
+package ch.lihsmi;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.junit.Assert.*;
+
+@Category(ch.lihsmi.IntegrationTests.class)
+public class DemoClassIntegrationTest {
+
+    @Test
+    public void assertSomething() {
+        assertSame(1,1);
+    }
+
+
+}
